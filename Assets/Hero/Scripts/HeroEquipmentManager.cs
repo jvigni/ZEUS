66213625b65a1498ea27a186b89163c8efa7030b
Juan Vigni <juan.vigni@gmail.com>
2024-03-27 21:22:48 -0300

    equipment manager inputs cleanardo

--- Assets/Hero/Scripts/HeroEquipmentManager.cs
+++ Assets/Hero/Scripts/HeroEquipmentManager.cs
@@ -7,22 +7,46 @@ public class HeroEquipmentManager : MonoBehaviour
     [SerializeField] List<Equipable> equipables;
     public Equipable Equiped;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        MouseInputs();
+        EquipmentSwapInputs();
+    }
+
+    void MouseInputs()
+    {
+        if (Equiped == null) return;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Equiped.LClickDown();
+        if (Input.GetKey(KeyCode.Mouse0))
+            Equiped.LClickPressed();
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+            Equiped.LClickUp();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Equiped.RClickDown();
+        if (Input.GetKey(KeyCode.Mouse0))
+            Equiped.RClickPressed();
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+            Equiped.RClickUp();
+    }
+
+    void EquipmentSwapInputs()
+    {
+        if (equipables.Count > 0 && Input.GetKeyDown(KeyCode.Alpha1))
             Equip(equipables[0]);
 
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        else if (equipables.Count > 1 && Input.GetKeyDown(KeyCode.Alpha2))
             Equip(equipables[1]);
 
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        else if (equipables.Count > 2 && Input.GetKeyDown(KeyCode.Alpha3))
             Equip(equipables[2]);
 
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        else if (equipables.Count > 3 && Input.GetKeyDown(KeyCode.Alpha4))
             Equip(equipables[3]);
 
-        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        else if (equipables.Count > 4 && Input.GetKeyDown(KeyCode.Alpha5))
             Equip(equipables[4]);
     }
 
